156927b894cbd3e3cf2716a689710347ea36993d
imtiyaj786 <imtiyaj1523@gmail.com>
2023-04-11 11:32:04 +0530

    Movie List&Card Movie Endpoint, Cool hover effect

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,10 @@
 import useCurrentUser from "@/hooks/useCurrentUser";
 import { NextPageContext } from "next";
 import { getSession, signOut } from "next-auth/react";
-import Navbar from "@/components/navbar";
+import Navbar from "@/components/Navbar";
+import Billboard from "@/components/Billboard";
+import MovieList from "@/components/MovieList";
+import useMovieList from "@/hooks/useMovieList";
 
 export async function getServerSideProps(context: NextPageContext) {
   const session = await getSession(context);
@@ -20,16 +23,14 @@ export async function getServerSideProps(context: NextPageContext) {
 }
 
 export default function Home() {
-  const { data: user } = useCurrentUser();
+  const { data: movie = [] } = useMovieList();
   return (
     <>
       <Navbar />
-      {/* <h1 className="text-4xl text-green-500">Netflix Clone</h1>
-      <p className="text-white"> Logged in as : {user?.name}</p>
-      <button className="h-10 w-full bg-white" onClick={() => signOut()}>
-        {" "}
-        Logout!
-      </button> */}
+      <Billboard />
+      <div className="pb-40">
+        <MovieList title="Trending Now" data={movie} />
+      </div>
     </>
   );
 }
